bab067d67297842af363e2d458ef13787cbc2354
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2023-08-06 13:35:13 +0200

    Added multiply
    Co-authored-by: Krzysztof Zawisła <zawislakrzysztof.zk@gmail.com>

--- src/app.rs
+++ src/app.rs
@@ -55,16 +55,34 @@ pub async fn rates() -> Result<String, ServerFnError> {
 }
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
-    let (value, set_value) = create_signal(cx, String::new());
+    let (value, set_value) = create_signal(cx, 0 as f32);
+    let (quantity, set_quantity) = create_signal(cx, 0 as f32);
+    let (multiply_score, set_multiply_score) = create_signal(cx, 0 as f32);
+    create_effect(cx, move |_| {
+        set_multiply_score(quantity.get() * value.get());
+    });
     view! { cx,
         <button on:click=move |_| {
             spawn_local(async move {
                 let text = rates().await.unwrap();
-                set_value(text)
+                let text = text.replace(",", ".");
+                let value = text.parse::<f32>();
+            match value {
+                Ok(value) => set_value(value),
+                Err(_) => {},
+            };
             });
         }>
-            "Add Todo"
+            "Check rate"
             {value}
         </button>
+        <input id="quantity" min="0" type="number" placeholder="Paste here your quantity of shares" on:input=move |event| {
+            let value = event_target_value(&event);
+            let value = value.parse::<f32>();
+            match value {
+                Ok(value) => set_quantity(value),
+                Err(_) => {},
+            };} />
+        <div>{multiply_score}</div>
     }
 }
