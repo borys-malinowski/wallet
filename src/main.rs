5d41cbea198b43be2abcdda1c382adcde6b9ef0f
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2023-11-12 13:02:38 +0100

    Fix

--- src/main.rs
+++ src/main.rs
@@ -74,10 +74,14 @@ async fn main() {
 #[cfg(feature = "ssr")]
 #[cfg(not(feature = "ssr"))]
 pub fn main() {
+    print!("test");
     // no client-side main function
     // unless we want this to work with e.g., Trunk for a purely client-side app
     // see lib.rs for hydration function instead
 }
+
+
+
 
 
 
